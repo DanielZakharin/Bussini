52a75abe5d7533611ba94bbb21d923a7675368f9
Daniel Z <daniel.zakharin@gmail.com>
2023-01-22 00:03:34 +0200

    WIP stop departures

--- app/src/main/java/fi/danielz/hslbussin/presentation/stopdisplay/model/StopDeparturesDataSource.kt
+++ app/src/main/java/fi/danielz/hslbussin/presentation/stopdisplay/model/StopDeparturesDataSource.kt
@@ -1,9 +1,56 @@
 package fi.danielz.hslbussin.presentation.stopdisplay.model
 
+import com.apollographql.apollo3.ApolloClient
+import fi.danielz.hslbussin.StopDeparturesQuery
+import fi.danielz.hslbussin.StopQuery
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.*
+import javax.inject.Inject
+
 interface StopSingleDepartureData {
     val departureTime: Long
 }
 
-interface StopDeparturesData {
-    val departures: List<StopSingleDepartureData>
+class StopSingleDepartureQueryData(queryDataItem: StopDeparturesQuery.StopTimesForPattern) :
+    StopSingleDepartureData {
+
+    override val departureTime: Long = queryDataItem.let {
+        //val departureOffset: Long = if(it.realtime == true) it.realtimeDeparture else it.scheduledDeparture
+        // start of day + departure time
+        //it.serviceDay + departureOffset
+        //departureOffset
+        // FIXME apollo types are all wonky?
+        5L
+    }
+}
+
+interface StopDeparturesDataSource {
+    fun departuresForStopAndPattern(
+        stopId: String,
+        patternId: String
+    ): Flow<List<StopSingleDepartureData>>
+
+    val errors: Flow<List<com.apollographql.apollo3.api.Error>>
+}
+
+class StopDeparturesNetworkDataSource @Inject constructor(private val apolloClient: ApolloClient) :
+    StopDeparturesDataSource {
+
+    private val stopIdFlow = MutableStateFlow<String?>(null)
+    private val patternIdFlow = MutableStateFlow<String?>(null)
+    private val stopDeparturesFlow =
+        stopIdFlow.combine(patternIdFlow, ::Pair).flatMapConcat { (stopId, patternId) ->
+            if (stopId == null || patternId == null) return@flatMapConcat flow { }
+            apolloClient.query(StopQuery(stopId, patternId)).toFlow()
+        }
+
+    override fun departuresForStopAndPattern(
+        stopId: String,
+        patternId: String
+    ): Flow<List<StopSingleDepartureData>> {
+        stopIdFlow.value = stopId
+        patternIdFlow.value = patternId
+        return stopDeparturesFlow.mapNotNull {
+        }
+    }
 }

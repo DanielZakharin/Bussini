3fb57601841eaf69457ffeee4dd7001cc44de999
Daniel Z <daniel.zakharin@gmail.com>
2023-03-26 12:08:50 +0300

    RouteDataSource with inject coroutine scope

--- app/src/main/java/fi/danielz/hslbussin/presentation/routeselection/di/RoutesDataSourceModule.kt
+++ app/src/main/java/fi/danielz/hslbussin/presentation/routeselection/di/RoutesDataSourceModule.kt
@@ -1,18 +1,25 @@
 package fi.danielz.hslbussin.presentation.routeselection.di
 
+import android.app.Activity
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
 import fi.danielz.hslbussin.presentation.routeselection.model.RoutesDataSource
 import fi.danielz.hslbussin.presentation.routeselection.model.RoutesNetworkDataSource
+import kotlinx.coroutines.CoroutineScope
+import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
 
 
 @Module
-@InstallIn(ActivityComponent::class, ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 abstract class RoutesDataSourceModule {
     @Binds
+    @Singleton
     abstract fun provideRoutesDataSource(routesNetworkDataSource: RoutesNetworkDataSource): RoutesDataSource
 
 }

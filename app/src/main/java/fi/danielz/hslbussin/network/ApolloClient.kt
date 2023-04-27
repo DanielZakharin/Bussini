95d9483ca7c26c36392c77ee4d75920681e85f8d
Daniel Z <daniel.zakharin@gmail.com>
2023-04-27 18:01:17 +0300

    Network result model

--- app/src/main/java/fi/danielz/hslbussin/network/ApolloClient.kt
+++ app/src/main/java/fi/danielz/hslbussin/network/ApolloClient.kt
@@ -1,18 +1,23 @@
 package fi.danielz.hslbussin.network
 
 import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.Query
 import com.apollographql.apollo3.network.okHttpClient
 import dagger.Provides
 import fi.danielz.hslbussin.BuildConfig
 import okhttp3.OkHttpClient
+import timber.log.Timber
 import javax.inject.Singleton
 
-private const val apiUrl = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql"
-
-private val okHttpClient = OkHttpClient.Builder().build()
-
-val apolloClient = ApolloClient.Builder()
-    .okHttpClient(okHttpClient)
-    .serverUrl(apiUrl)
-    .addHttpHeader("digitransit-subscription-key", BuildConfig.API_KEY)
-    .build()
+suspend fun <D: Query.Data> ApolloClient.queryAsNetworkResponse(query: Query<D>): NetworkStatus<D> {
+    return try {
+        val res = query(query).execute()
+        NetworkStatus.Success(
+            // TODO how to get actual status code from apollo
+            res.dataAssertNoErrors
+        )
+    } catch (e: Exception) {
+        Timber.e(e)
+        NetworkStatus.Error(e)
+    }
+}

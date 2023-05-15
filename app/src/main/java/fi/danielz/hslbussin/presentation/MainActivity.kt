935d099eec71a2f412ad9ce940faa6bdd902fbd5
Daniel Z <daniel.zakharin@gmail.com>
2023-05-15 17:27:20 +0300

    Removed back button headers, fixed navigation pop logic

--- app/src/main/java/fi/danielz/hslbussin/presentation/MainActivity.kt
+++ app/src/main/java/fi/danielz/hslbussin/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package fi.danielz.hslbussin.presentation
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import fi.danielz.hslbussin.R
 
@@ -10,5 +11,16 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        val navCtrl = findNavController(R.id.navHost)
+        val isLastFragment =
+            navCtrl.previousBackStackEntry?.destination == null
+        if (isLastFragment) {
+            super.onBackPressedDispatcher.onBackPressed()
+        } else {
+            navCtrl.popBackStack()
+        }
     }
 }

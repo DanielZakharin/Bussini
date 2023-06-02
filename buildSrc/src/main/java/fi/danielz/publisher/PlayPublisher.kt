b3028fee7344455d0e150cc97a807e62b85ab270
Daniel Z <daniel.zakharin@gmail.com>
2023-06-02 12:42:28 +0300

    WIP gradle task publish, borked dependencies

--- buildSrc/src/main/java/fi/danielz/publisher/PlayPublisher.kt
+++ buildSrc/src/main/java/fi/danielz/publisher/PlayPublisher.kt
@@ -1,4 +1,4 @@
-package fi.bussini.publishing
+package fi.danielz.publisher
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.http.FileContent
@@ -13,28 +13,30 @@ import com.google.api.services.androidpublisher.model.TrackRelease
 import com.google.auth.http.HttpCredentialsAdapter
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.auth.oauth2.ServiceAccountCredentials
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
 import java.io.File
 import java.io.FileInputStream
 
 private const val WEAR_OS_PROD_TRACK = "wear:production"
 
 // from https://github.com/stasheq/google-play-apk-upload/blob/main/apk_upload/src/main/kotlin/me/szymanski/apkupload/Main.kt
-object PlayStorePublisher {
-    @JvmStatic
-    fun main(args: Array<String>) {
+class PlayStorePublisherTask: DefaultTask() {
+    @TaskAction
+    fun publishProdRelease() {
         try {
             println("Started upload script!")
-            val appId = args[0]
-            val apkPath = args[1]
-            val credentialsPath = args[2]
-            println("Received arguments\nappId: $appId\napkPath: $apkPath\ncredentialsPath: $credentialsPath")
+            val appId = "fi.danielz.bussini"
+            val credentialsPath = "${identityPath.path}/publishKeyBussini.json"
+            val bundlePath = "${identityPath.path}/app/build/outputs/bundle/release/app-release.aab"
+            println("Running with params\nappId: $appId\napkPath: $bundlePath\ncredentialsPath: $credentialsPath")
 
             val credentials = authenticate(credentialsPath)
             println("Succesfully authenticated with credentials")
             val androidPublisher = makePublisher(credentials)
             println("Succesfully instantiated AndroidPublisher")
 
-            val bundleVersionNumber = uploadBundle(appId, apkPath, androidPublisher)
+            val bundleVersionNumber = uploadBundle(appId, bundlePath, androidPublisher)
             updateTrack(androidPublisher, appId, bundleVersionNumber)
             println("Success! New release created")
         } catch (e: Exception) {
